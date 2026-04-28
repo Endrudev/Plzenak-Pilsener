32ed5575b2b808c796c767a16f53675418984e48
Endrudev <ondra.stindl@gmail.com>
2026-04-28 19:12:08 +0200

    Základní struktura
    Vytvoření základních souboru pomocí Vite a struktury pro React.js aplikaci.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <main>
+            <h1>Event Hub</h1>
+            <p>Vite + React kostra projektu je připravená.</p>
+        </main>
+    );
+}
+
+export default App;
